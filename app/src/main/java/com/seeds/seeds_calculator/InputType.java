3e5842265c19083880556eb0454d836b2765bbb2
Saarhin <sarah77.amini@gmail.com>
2018-07-20 23:51:49 +0430

    Delete button completed

--- app/src/main/java/com/seeds/seeds_calculator/InputType.java
+++ app/src/main/java/com/seeds/seeds_calculator/InputType.java
@@ -11,6 +11,8 @@ import java.util.Set;
 public abstract class InputType {
     public static String input="$$|$$";
     public static Set<String> leftRightKey= new HashSet<>();
+    public static Set<String> deletekey= new HashSet<>();
+    public static Set<String> deleteSkipKey= new HashSet<>();
 
     public String getInput(){
         return input;
--- app/src/main/java/com/seeds/seeds_calculator/Pure.java
+++ app/src/main/java/com/seeds/seeds_calculator/Pure.java
@@ -1,9 +1,11 @@
 package com.seeds.seeds_calculator;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Pure extends InputType {
@@ -18,14 +20,16 @@ public class Pure extends InputType {
             case R.id.c_top:
             case R.id.c_left:
                 int leftSkipNumber=findLeftSkipNumber(input,leftRightKey);
-                cursorPosition=findCursorPosition(input);
-                input=input.substring(0,cursorPosition-1-leftSkipNumber)+"|"+input.substring(cursorPosition-1-leftSkipNumber,cursorPosition-1)+input.substring(cursorPosition);
+                input=handleLeftSkip(leftSkipNumber);
                 break;
             case R.id.c_buttom:
             case R.id.c_right:
                 int rightSkipNumber=findRightSkipNumber(input,leftRightKey);
                 cursorPosition=findCursorPosition(input);
                 input=input.substring(0,cursorPosition-1)+input.substring(cursorPosition,cursorPosition+rightSkipNumber)+"|"+input.substring(cursorPosition+rightSkipNumber);
+                break;
+            case R.id.but31:
+                input=handleDelete(input,deletekey,leftRightKey,context);
                 break;
             case R.id.but4:
                 String modeText="1.COMP\t2.CMPLX\n3.STAT\t4.BASE-N\n5.EQN-1\t6.MATRIX\n7.TABLE\t8.VECTOR";
@@ -45,7 +49,6 @@ public class Pure extends InputType {
     }
 
 
-
     private String findString(View v, int previousButton) {
         switch(v.getId()){
             case R.id.but43:
@@ -53,11 +56,13 @@ public class Pure extends InputType {
             case R.id.but44:
                 return ".";
             case R.id.but45:
+                deletekey.add("\\times{10}^{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\times{10}^{(");
                 return "\\times{10}^{()}";
             case R.id.but38:
                 if(previousButton==R.id.but18) {
+                    deletekey.add("\\sinh{()}");
                     leftRightKey.add(")}");
                     leftRightKey.add("\\sinh{(");
                     return "\\sinh{()}";
@@ -65,6 +70,7 @@ public class Pure extends InputType {
                 return "1";
             case R.id.but39:
                 if(previousButton==R.id.but18) {
+                    deletekey.add("\\cosh{()}");
                     leftRightKey.add(")}");
                     leftRightKey.add("\\cosh{(");
                     return "\\cosh{()}";
@@ -72,6 +78,7 @@ public class Pure extends InputType {
                 return "2";
             case R.id.but40:
                 if(previousButton==R.id.but18) {
+                    deletekey.add("\\tanh{()}");
                     leftRightKey.add(")}");
                     leftRightKey.add("\\tanh{(");
                     return "\\tanh{()}";
@@ -79,10 +86,17 @@ public class Pure extends InputType {
                 return "3";
             case R.id.but41:
                 return"+";
+            case R.id.but17:
+                deletekey.add("{()}^{\\square}");
+                leftRightKey.add("{(");
+                leftRightKey.add("{()}^{\\square}");
+                return "{()}^{\\square}";
+            case R.id.but16:
             case R.id.but42:
                 return "-";
             case R.id.but33:
                 if(previousButton==R.id.but18) {
+                    deletekey.add("arcsinh{()}");
                     leftRightKey.add(")}");
                     leftRightKey.add("arcsinh{(");
                     return "arcsinh{()}";
@@ -90,6 +104,7 @@ public class Pure extends InputType {
                 return "4";
             case R.id.but34:
                 if(previousButton==R.id.but18) {
+                    deletekey.add("arccosh{()}");
                     leftRightKey.add(")}");
                     leftRightKey.add("arccosh{(");
                     return "arccosh{()}";
@@ -97,15 +112,18 @@ public class Pure extends InputType {
                 return "5";
             case R.id.but35:
                 if(previousButton==R.id.but18) {
+                    deletekey.add("arctanh{()}");
                     leftRightKey.add(")}");
                     leftRightKey.add("arctanh{(");
                     return "arctanh{()}";
                 }
                 return "6";
             case R.id.but36:
+                deletekey.add("\\times");
                 leftRightKey.add("\\times");
                 return "\\times";
             case R.id.but37:
+                deletekey.add("\\div");
                 leftRightKey.add("\\div");
                 return "\\div";
             case R.id.but28:
@@ -119,54 +137,66 @@ public class Pure extends InputType {
             case R.id.but25:
                 return ")";
             case R.id.but19:
+                deletekey.add("\\sin{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\sin{(");
                 return "\\sin{()}";
             case R.id.but20:
+                deletekey.add("\\cos{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\cos{(");
                 return "\\cos{()}";
             case R.id.but21:
+                deletekey.add("\\tan{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\tan{(");
                 return "\\tan{()}";
             case R.id.but10:
+                deletekey.add("\\frac{()}{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add(")}{(");
                 leftRightKey.add("\\frac{(");
                 return "\\frac{()}{()}";
             case R.id.but11:
+                deletekey.add("\\sqrt{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\sqrt{(");
                 return "\\sqrt{()}";
             case R.id.but12:
+                deletekey.add("{()}^2");
                 leftRightKey.add("{(");
                 leftRightKey.add(")}^2");
                 return"{()}^2";
             case R.id.but13:
+                deletekey.add("{()}^{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("{(");
                 leftRightKey.add(")}^{(");
                 return "{()}^{()}";
             case R.id.but14:
+                deletekey.add("\\log{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\log{(");
                 return "\\log{()}";
             case R.id.but15:
+                deletekey.add("\\ln{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add("\\ln{(");
                 return "\\ln{()}";
             case R.id.but7:
+                deletekey.add("\\int_{()}^{()}{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add(")}{(");
                 leftRightKey.add(")}^{(");
                 leftRightKey.add("\\int_{(");
                 return "\\int_{()}^{()}{()}";
             case R.id.but8:
+                deletekey.add("{()}^{-1}");
                 leftRightKey.add("{(");
                 leftRightKey.add(")}^{-1}");
                 return "{()}^{-1}";
             case R.id.but9:
+                deletekey.add("\\log_{()}{()}");
                 leftRightKey.add(")}");
                 leftRightKey.add(")}{(");
                 leftRightKey.add("\\log_{(");
@@ -182,9 +212,11 @@ public class Pure extends InputType {
             return 0;
         int skipNumber=1;
         for(String str: leftRightKey){
-            if(input.substring(cursorPosition-1-str.length(),cursorPosition-1).equals(str)){
-                if(str.length()>skipNumber){
-                    skipNumber=str.length();
+            if(cursorPosition-1-str.length()>=0) {
+                if (input.substring(cursorPosition - 1 - str.length(), cursorPosition - 1).equals(str)) {
+                    if (str.length() > skipNumber) {
+                        skipNumber = str.length();
+                    }
                 }
             }
         }
@@ -196,13 +228,86 @@ public class Pure extends InputType {
             return 0;
         int skipNumber=1;
         for(String str: leftRightKey){
-            if(input.substring(cursorPosition,cursorPosition+str.length()).equals(str)){
-                if(str.length()>skipNumber){
-                    skipNumber=str.length();
+            if(cursorPosition+str.length()<input.length()-1) {
+                if (input.substring(cursorPosition, cursorPosition + str.length()).equals(str)) {
+                    if (str.length() >= skipNumber) {
+                        skipNumber = str.length();
+                    }
                 }
             }
         }
         return skipNumber;
+    }
+
+    private String handleDelete(String input, Set<String> deletekey, Set<String> leftRightKey, Context context) {
+        int cursorPosition=findCursorPosition(input);
+        for(String str:deletekey){
+            ArrayList<Integer> indexes=new ArrayList<>();
+            indexes=exist(str,input);
+            if(indexes!=null){
+                for(Integer i:indexes){
+                    input=input.substring(0,i)+input.substring(i+1);
+
+                }
+
+                return input;
+            }
+        }
+        int leftSkipNumber=findLeftSkipNumber(input,leftRightKey);
+        if(leftSkipNumber>1){
+            input=handleLeftSkip(leftSkipNumber);
+            return input;
+        }
+        if(cursorPosition>3)
+            input=input.substring(0,cursorPosition-2)+input.substring(cursorPosition-1);
+        return input;
+    }
+
+    private ArrayList<Integer> exist(String str, String input) {
+        ArrayList<Integer> indexes=new ArrayList<>();
+        int cursorPosition=findCursorPosition(input);
+        int strLength=str.length()-1;
+        int pCheck=0;
+        if(input.toCharArray()[cursorPosition-2]==str.toCharArray()[strLength]){
+            indexes.add(cursorPosition-2);
+            indexes.add(cursorPosition-3);
+            strLength-=2;
+            for(int i=cursorPosition-4;i>=0 && strLength>=0;i--){
+                if(input.toCharArray()[i]==str.toCharArray()[strLength]){
+                    if(input.toCharArray()[i]=='{'||input.toCharArray()[i]=='('){
+                        if(pCheck==0){
+                            indexes.add(i);
+                            strLength--;
+                        }
+                        else
+                            pCheck--;
+                    }
+
+                    else {
+                        indexes.add(i);
+                        strLength--;
+                    }
+                }
+
+                else if (input.toCharArray()[i]=='}'||input.toCharArray()[i]==')'){
+                    pCheck++;
+                }
+                else if(input.toCharArray()[i]=='{'||input.toCharArray()[i]=='('){
+                    pCheck--;
+                }
+
+
+            }
+        }
+        if(indexes.size()==str.length())
+            return indexes;
+        return null;
+    }
+
+    private String handleLeftSkip(int leftSkipNumber) {
+        int cursorPosition=findCursorPosition(input);
+        input=input.substring(0,cursorPosition-1-leftSkipNumber)+"|"+input.substring(cursorPosition-1-leftSkipNumber,cursorPosition-1)+input.substring(cursorPosition);
+        return input;
     }
 
 
